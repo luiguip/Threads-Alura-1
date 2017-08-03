64597609425eaabc4ede453574f0bc1295ce9692
luiguip <luiguip@github.com>
2017-08-03 15:28:22 -0300

    Threads alura lesson 1

---
+++ src/alura/threads/Principal.java
@@ -0,0 +1,10 @@
+package alura.threads;
+
+public class Principal {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("This is a thread");
+		Thread.sleep(300000);
+	}
+	
+}
